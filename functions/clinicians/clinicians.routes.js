4bc7d9e5bbfc355cb8fb3f130c817fb6c1e057b5
John Kramer <john.kramer@kinandcarta.com>
2022-02-23 13:54:11 -0500

    Added validation to Clinician model

--- functions/clinicians/clinicians.routes.js
+++ functions/clinicians/clinicians.routes.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const { checkSchema, validationResult } = require('express-validator');
 const { createClinician, deleteClinician, getClinician, getClinicians, updateClinician, updateClinicianWithPatient, removePatientFromClinician } = require('./clinicians.controllers');
+const { Clinician } = require('./clincian.model');
+
+const validateClinician = async (req, res, next) => {
+  await Promise.all(checkSchema(Clinician.schema).map((validation) => validation.run(req)));
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+  }
+
+  return next;
+};
+
 const router = express.Router();
 
 router.route('/')
   .get(getClinicians)
-  .post(createClinician);
+  .post(validateClinician, createClinician);
 
 router.route('/:id')
   .delete(deleteClinician)
   .get(getClinician)
-  .put(updateClinician);
+  .put(validateClinician, updateClinician);
 
 router.route('/addPatient/:id')
   .put(updateClinicianWithPatient);
